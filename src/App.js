10f10e0c86b89418aa235ba27c762a90366ce4a8
Bae <Bae@songjingeun-ui-MacBookPro.local>
2021-05-22 18:35:05 +0900

    react-route-dom을 활용한 페이지 이동

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+import Rooms from './rooms';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return (
+        <BrowserRouter>
+          <div style={{padding: 20, border: '5px solid gray'}}>
+            <Link to="/" >홈</Link>
+            <br />
+            <Link to="/photo" >사진</Link>
+            <br />
+            <Link to="/rooms" >방 소개</Link>
+            <br />
+            <Route exact path="/" component={Home} />
+            <Route path="/photo" component={Photo} />
+            <Route path="/rooms" component={Rooms} />
+          </div>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+function Home () {
+  return <h2>Hoem</h2>
+}
+function Photo () {
+  return <h2>Photo</h2>
+}
